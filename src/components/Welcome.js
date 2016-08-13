f698db96f566ffb9f44a488de50671b94caa6b8c
Vladimir Iakovlev <nvbn.rm@gmail.com>
2016-08-13 10:56:20 +0300

    Add landing page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,42 +1,64 @@
 import React, { Component } from 'react';
-import { h1 } from 'react-dom';
-import { TextField, RaisedButton } from 'material-ui';
+import { strong, p, iframe } from 'react-dom';
+import { AppBar, FlatButton, Card, CardActions, CardHeader, CardText } from 'material-ui';
 
 class Welcome extends Component {
-  _onKeyDown({keyCode}) {
-    if (keyCode == 13) {
-      this._goSession();
-    }
-  }
-
-  _goSession() {
-    this.props.goSession(this._input.input.value);
-  }
-
   render() {
     return (
-      <div style={styles.container}>
-        <h1 style={styles.title}>Welcome to Code View!</h1>
-        <div>
-          <TextField ref={(input) => this._input = input}
-                     onKeyDown={this._onKeyDown.bind(this)}/>
-          {" "}
-          <RaisedButton onClick={this._goSession.bind(this)}
-                        primary={true}>
-            Go!
-          </RaisedButton>
-        </div>
+      <div>
+        <AppBar title="code-view.io [not-even-alpha]"
+                showMenuIconButton={false}
+                iconElementRight={
+                  <FlatButton label="github"
+                              href="https://github.com/code-view/"
+                              target="_blank"/>
+                }/>
+
+        <Card style={styles.card}>
+          <CardHeader title="What is code-view.io?"/>
+          <CardText>
+            <p>It's a service for streaming code from IDE to browser.
+              Useful in review process, workshops and other situations.</p>
+
+            <iframe style={styles.video} width="800" height="450"
+                    src="https://www.youtube.com/embed/nDPBLFR6vyQ"></iframe>
+          </CardText>
+        </Card>
+
+        <Card style={styles.card}>
+          <CardHeader title="How to use it?"/>
+          <CardText>
+            <p>You just need to install plugin for your IDE and start streaming.</p>
+
+            <p>Currently only IntelliJ IDEA 2016.2.0 supported.</p>
+
+            <img style={styles.video} src="/intellij.png" alt=""/>
+          </CardText>
+          <CardActions>
+            <FlatButton label="Download IntelliJ IDEA plugin"
+                        href="https://github.com/code-view/client_idea/releases"
+                        target="_blank"/>
+            <FlatButton label="API documentation"
+                        href="https://github.com/code-view/server#api"
+                        target="_blank"/>
+          </CardActions>
+        </Card>
       </div>
     );
   }
 }
 
 const styles = {
-  container: {
-    textAlign: 'center'
+  card: {
+    maxWidth: '800px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '30px',
   },
-  title: {
-    fontSize: '40px'
+  video: {
+    border: 0,
+    marginLeft: '-16px',
+    marginBottom: '-19px'
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './containers/app';
 
+injectTapEventPlugin();
 ReactDOM.render(<App />, document.getElementById('main'));
-
